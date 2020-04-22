8c655d5eb1c20819934e7e0e886da02b81022a21
Tripti Chhetri <Tripti@365power.dev>
2020-04-22 01:14:15 -0500

    Add project files.

---
+++ Movies/Movies.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Movies
+{
+    class Movies
+    {
+        public string name;
+        public string director;
+        private string rating;
+        public Movies (string aName, string aDirector, string aRating)
+        {
+            name = aName;
+            director = aDirector;
+            Rating = aRating;
+
+        }
+
+        public Movies()
+        {
+
+        }
+
+
+        public string Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+
+        public string result = "";
+        /// <summary>
+        /// checkRating method checks the rating of the movie and suggest users
+        /// </summary>
+        /// <returns></returns>
+      public string checkRating()
+        {
+            switch (Rating)
+            {
+                case "PG":
+                    result = ("This movie requires Parental Guidance");
+                    break;
+                case "PG-13":
+                    result = ("In this movie, some material may be inappropriate for children under 13");
+                    break;
+                case "R":
+                    result = ("This movie requires anyone under 17 to accompany parent or adult guardian");
+                    break;
+                case "X":
+                    result = ("No one under 17 admitted");
+                    break;
+                default:
+                    result = ("This film does not have a rating");
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Movies/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movies shrek = new Movies("Shrek", "Tom Smith", "PG");
+            Movies Ishq = new Movies("Titanic", "Jack Ryan", "PG-13");
+            shrek.Rating = "PG-13";
+            Console.WriteLine(shrek.Rating);
+            Console.WriteLine(shrek.checkRating());
+
+            List<Movies> movieList = new List<Movies>();
+
+            movieList.Add(shrek);
+            movieList.Add(Ishq);
+            movieList.Add(new Movies("Two States", "Abhisek Verman", "PG-13"));
+            movieList.Add(new Movies("Three Idiots", "Raju Hirani", "PG"));
+            movieList.Add(new Movies("Munnabhai MBBS", "Raju Hirani", "PG-13"));
+
+            foreach (var movie in movieList)
+            {
+                Console.WriteLine(movie.name);
+                Console.WriteLine(movie.checkRating());
+            }
+	{
+
+	}
+            Console.ReadLine();
+        }
+    }
+}
